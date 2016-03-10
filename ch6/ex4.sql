fc0751e235e19c0540a65b4cbda6fb9c1222e794
Christopher Natke <cgnatke@gmail.com>
2016-03-10 03:26:07 -0500

    work on ch 6

---
+++ ch6/ex4.sql
@@ -0,0 +1,18 @@
+
+
+SELECT custid, empid
+FROM Sales.Orders
+WHERE orderdate >= '1/1/2008' AND orderdate < '2/1/2008'
+
+INTERSECT
+
+SELECT custid, empid
+FROM Sales.Orders
+WHERE  orderdate >= '2/1/2008' AND orderdate < '3/1/2008'
+
+EXCEPT
+
+SELECT custid, empid
+FROM Sales.Orders
+WHERE  YEAR(orderdate) = '2007'
+
---
+++ ch6/ex5.sql
@@ -0,0 +1,10 @@
+SELECT custid, empid
+FROM Sales.Orders
+WHERE orderdate >= '1/1/2008' AND orderdate < '2/1/2008'
+
+INTERSECT
+
+SELECT custid, empid
+FROM Sales.Orders
+WHERE  orderdate >= '2/1/2008' AND orderdate < '3/1/2008'
+
